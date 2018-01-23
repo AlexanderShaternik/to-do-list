3d03c2d5a8e78f41d876f8273be51d6b2f81482a
afomina <Anastasia.Fomina@ihs.com>
2018-01-23 16:09:32 +0300

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -17,59 +17,54 @@ let count = 0;
 let todo = [];
 let out = JSON.parse(localStorage.getItem("todo"));
 console.log("out" ,out);
+console.log("todo" ,todo);
 
-function Task(content,date,checked,id) {
-	this.content = content,
-	this.date    = date,
-	this.checked = checked
-	this.id      = count++
+function Task(content,date,isChecked,id) {
+	this.content   = content,
+	this.date      = date,
+	this.isChecked = isChecked,
+	this.id        = count
 }
+
 // unload from localSrorage
 if(out != null ) { 
+	load()
+}
+
+function load(){
+	let task_container 	= document.querySelector('.task-container');
 	for (let i = 0; i < out.length; i++) {
-		let field 		    = document.querySelector('.add-task');
-		let content 	    = field.value;
-		let task_container 	= document.querySelector('.task-container');
-		let date            = date_now.value;
-		let arr_delete_task = document.querySelectorAll('.delete-task-btn');
-
-		content = out[i].content;
-		date = out[i].date ;
-		let checked = false;
-		checked = out[i].checked;
-
-		// let invalid = date.split('.');
-		// console.log(invalid);
-		// if(( +invalid[0] < 1) || (31 < +invalid[0])  || isNaN(+invalid[0])) break;
-		// if(( +invalid[1] < 1) || (12 < +invalid[1]) || isNaN(+invalid[1])) break;
-		// if((2018> +invalid[2]) || isNaN(+invalid[2])) break;
-		// if ( content == "" || date=="" ) break;
 
 		let task_box   = document.createElement('div');
 		let input      = document.createElement('input');
 		let task       = document.createElement('p');
 		let task_date  = document.createElement('p');
 		let button_del = document.createElement('button');
+
+		content = out[i].content;
+		date = out[i].date ;
+		isChecked = out[i].isChecked;
+		
 		button_del.classList.add('delete-task-btn');
 		task_box.classList.add('task-box');
 		input.setAttribute('type','checkbox');
 		task.textContent = content; 
 		task_date.textContent =date;
 		button_del.textContent = 'Удалить';
+		// add data-id
+		
+		count = i;
 		task_box.dataset.id = count;
+
 		// fill task_box with contents
 		task_box.appendChild(input);
 		task_box.appendChild(button_del);
 		task_box.appendChild(task);
 		task_box.appendChild(task_date);
-			
 		// console.log(task_box);
-
 		task_container.appendChild(task_box);
-		field.value = '';
-		date_now.value = '';
 			
-		if (out[i].checked){
+		if (out[i].isChecked){
 			input.checked = true;
 			task_box.classList.add('active');
 		} else {
@@ -79,13 +74,8 @@ if(out != null ) {
 		input.addEventListener('click', checkTask);
 		button_del.addEventListener('click', removeTask);
 		
-		let obj = new Task(content,date,checked);
-		todo.push(obj);
-		console.log("todo",todo);
-		localStorage.setItem("todo",JSON.stringify(todo));
 	}
-			
-}
+}		
 
 function addTask() {
 	// content from input.add-task, .date
@@ -94,45 +84,27 @@ function addTask() {
 	let task_container 	= document.querySelector('.task-container');
 	let date            = date_now.value;
 	let arr_delete_task = document.querySelectorAll('.delete-task-btn');
+	let isChecked = false;
 
-	// date check
+	task_container.innerHTML = "Задания";
+	// date Check
 	let invalid = date.split('.');
 	if(( +invalid[0] < 1) || (31 < +invalid[0])  || isNaN(+invalid[0])) return;
 	if(( +invalid[1] < 1) || (12 < +invalid[1]) || isNaN(+invalid[1])) return;
 	if((2018> +invalid[2]) || isNaN(+invalid[2])) return;
 	if ( content == "" || date=="" ) return;
 
-	// сreate task_box
-	let task_box   = document.createElement('div');
-	let input      = document.createElement('input');
-	let task       = document.createElement('p');
-	let task_date  = document.createElement('p');
-	let button_del = document.createElement('button');
-	button_del.classList.add('delete-task-btn');
-	task_box.classList.add('task-box');
-	input.setAttribute('type','checkbox');
-	task.textContent = content; 
-	task_date.textContent = date;
-	let checked = false;
-	button_del.textContent = 'Удалить';
-	task_box.dataset.id = count;
-	// fill task_box with contents
-	task_box.appendChild(input);
-	task_box.appendChild(button_del);
-	task_box.appendChild(task);
-	task_box.appendChild(task_date);
-
-	task_container.appendChild(task_box);
+	todo.content = content;
+	todo.date = date ;
+	todo.isChecked = isChecked;
 	field.value = '';
 	date_now.value = '';
-	
-	input.addEventListener('click', checkTask);
-	button_del.addEventListener('click', removeTask);
-	
-	let obj = new Task(content,date,checked);
+
+	let obj = new Task(content,date,isChecked);
 	todo.push(obj);
-	console.log(todo);
 	localStorage.setItem("todo",JSON.stringify(todo));
+	out = JSON.parse(localStorage.getItem("todo"));
+	load();
 }
 
 // delete task
@@ -148,6 +120,7 @@ function removeTask() {
 			break;
 		}
 	}
+
 	box.removeChild(parent);
 	localStorage.setItem("todo",JSON.stringify(todo));
 }
@@ -161,17 +134,18 @@ function checkTask() {
 	for (let i = 0; i < todo.length; i++) {
 		if(todo[i].id == id){
 			if (elem.checked){
-				todo[i].checked =true;
+				todo[i].isChecked =true;
 				parent.classList.add('active');
 
 			} else {
-				todo[i].checked = false;
+				todo[i].isChecked = false;
 				parent.classList.remove('active');
-
 			}
 		}
 	}	
+
 	localStorage.setItem("todo",JSON.stringify(todo));
 }
+
 
 
